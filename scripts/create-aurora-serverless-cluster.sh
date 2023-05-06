98f2e1a5117d4d3058387deda5125b33fd829636
nakamurakzz <kazutaka.nakamura@ncdc.co.jp>
2023-05-06 20:34:54 +0900

    serverless.yml作成

--- scripts/create-aurora-serverless-cluster.sh
+++ scripts/create-aurora-serverless-cluster.sh
@@ -3,9 +3,24 @@ echo "Create RDS cluster"
 REGION=ap-northeast-1
 echo "Region: $REGION"
 
+# select environment
+echo "select environment"
+echo "1: dev, 2: stg, 3: prod"
+read ENV_NUM
+if ENV_NUM == 1; then
+  ENV=dev
+elif ENV_NUM == 2; then
+  ENV=stg
+elif ENV_NUM == 3; then
+  ENV=prod
+else
+  echo "invalid number"
+  exit 1
+fi
+
 # .envからUSERNAME, COMPLEX_PASSWORDを読み込む
-USERNAME=$(grep USERNAME .env | cut -d '=' -f 2)
-COMPLEX_PASSWORD=$(grep COMPLEX_PASSWORD .env | cut -d '=' -f 2)
+USERNAME=$(grep USERNAME .env.$ENV | cut -d '=' -f 2)
+COMPLEX_PASSWORD=$(grep COMPLEX_PASSWORD .env.$ENV | cut -d '=' -f 2)
 
 echo $USERNAME
 echo $COMPLEX_PASSWORD
@@ -15,7 +30,7 @@ read PROFILE
 
 echo "Creating RDS cluster"
 
-aws rds create-db-cluster --db-cluster-identifier http-endpoint-test  --master-username $USERNAME \
+aws rds create-db-cluster --db-cluster-identifier cluster-endpoint-$ENV  --master-username $USERNAME \
 --master-user-password $COMPLEX_PASSWORD --engine aurora --engine-mode serverless \
 --region $REGION \
 --profile $PROFILE > /dev/null
@@ -24,7 +39,7 @@ echo "RDS cluster created"
 
 echo "Creating RDS cluster secret"
 aws secretsmanager create-secret \
-    --name HttpRDSSecret \
+    --name RDSSecret \
     --secret-string "{\"username\":\"$USERNAME\",\"password\":\"$COMPLEX_PASSWORD\"}" \
     --region $REGION \
     --profile $PROFILE > /dev/null
@@ -32,12 +47,12 @@ echo "RDS cluster secret created"
 
 # Get RDS Cluster ARN
 echo "Getting RDS cluster ARN"
-RDS_ARN=$(aws rds describe-db-clusters --db-cluster-identifier http-endpoint-test --region $REGION --profile $PROFILE | jq -r '.DBClusters[].DBClusterArn')
-RDS_CLUSTER_ID=$(aws rds describe-db-clusters --db-cluster-identifier http-endpoint-test --region $REGION --profile $PROFILE | jq -r '.DBClusters[].DBClusterIdentifier')
+RDS_ARN=$(aws rds describe-db-clusters --db-cluster-identifier cluster-endpoint-$ENV --region $REGION --profile $PROFILE | jq -r '.DBClusters[].DBClusterArn')
+RDS_CLUSTER_ID=$(aws rds describe-db-clusters --db-cluster-identifier cluster-endpoint-$ENV --region $REGION --profile $PROFILE | jq -r '.DBClusters[].DBClusterIdentifier')
 
 # Get RDS Cluster Secret ARN
 echo "Getting RDS cluster secret ARN"
-RDS_SECRET_ARN=$(aws secretsmanager describe-secret --secret-id HttpRDSSecret --region $REGION --profile $PROFILE | jq -r '.ARN')
+RDS_SECRET_ARN=$(aws secretsmanager describe-secret --secret-id RDSSecret --region $REGION --profile $PROFILE | jq -r '.ARN')
 
 echo "---------------------------------------------------"
 echo "RDS cluster ARN: $RDS_ARN"
@@ -52,7 +67,7 @@ aws rds modify-db-cluster \
   --region $REGION --profile $PROFILE > /dev/null
 echo "Data API enabled"
 
-RDS_DATA_API_ARN=$(aws rds describe-db-clusters --db-cluster-identifier http-endpoint-test --region $REGION --profile $PROFILE | jq -r '.DBClusters[].DBClusterArn')
+RDS_DATA_API_ARN=$(aws rds describe-db-clusters --db-cluster-identifier cluster-endpoint-$ENV --region $REGION --profile $PROFILE | jq -r '.DBClusters[].DBClusterArn')
 
 # Create database
 echo "Creating database"
@@ -66,5 +81,5 @@ echo "Creating table"
 aws rds-data execute-statement --resource-arn $RDS_DATA_API_ARN \
   --secret-arn $RDS_SECRET_ARN \
   --region $REGION --profile $PROFILE \
-  --sql "CREATE TABLE IF NOT EXISTS Pets(id varchar(200), type varchar(200), price float)" --database "TESTDB" > /dev/null
+  --sql "CREATE TABLE IF NOT EXISTS todos(id varchar(200), title varchar(200), completed boolean)" --database "TESTDB" > /dev/null
 echo "Table created"
